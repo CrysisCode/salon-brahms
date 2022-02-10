a84eedd327049f4f14daea884b66b6a260b89c6b
Tarik Topic <info@ttdigitalis.com>
2022-02-10 12:48:23 +0100

    firs-.designs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,51 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { GaugeModule } from 'angular-gauge';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatSelectModule } from '@angular/material/select';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CoverComponent } from './home/cover/cover.component';
+import { AboutComponent } from './home/about/about.component';
+import { GalleryComponent } from './home/gallery/gallery.component';
+import { PriceComponent } from './home/price/price.component';
+import { CalendarComponent } from './home/calendar/calendar.component';
+import { ContactComponent } from './home/contact/contact.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CoverComponent,
+    AboutComponent,
+    GalleryComponent,
+    PriceComponent,
+    CalendarComponent,
+    ContactComponent,
   ],
+  
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    GaugeModule,
+    MatTabsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
